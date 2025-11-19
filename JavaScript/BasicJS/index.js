c0bd1d1f21e8e8d6b732eefefa59e9a3432daae8
Abhishek1901Kumar <abhishek.error404@gmail.com>
2025-11-19 18:22:13 +0530

    Enquiry Form RICR

---
+++ JavaScript/BasicJS/index.js
@@ -0,0 +1,19 @@
+var a = 10;
+
+var a = 20;
+
+let b = 10;
+
+console.log(a, b);
+
+let ar = ["Abc", 1235, 45.67, "a", true];
+console.log(ar);
+
+console.warn("this is warning");
+// console.error("ye error hai");
+alert("this is alert");
+if (confirm("Are you sure")) {
+  console.log("Thanks");
+} else {
+  console.error("Its OK");
+}
---
+++ JavaScript/LoginRegisterPage/index.js
@@ -0,0 +1,13 @@
+function login(){
+    console.log("Login Button Clicked")
+    console.log("password :" +ps)
+
+    alert("Login Done");
+
+    document.getElementById("LoginEmail").value = "";
+    document.getElementById("LoginPassword").value = "";
+}
+
+function registration(){
+    console.log("Registration Buttton Clicked")
+}
