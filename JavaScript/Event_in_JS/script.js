d9a9e7e4a0d91c43f7609754d9546955d400a5f7
Abhishek1901Kumar <abhishek.error404@gmail.com>
2025-12-05 20:17:49 +0530

    Event Listener in JavaScript

--- JavaScript/Event_in_JS/script.js
+++ JavaScript/Event_in_JS/script.js
@@ -1,25 +1,94 @@
-function on(){
+function On() {
     document.getElementById("bulb").style.backgroundColor = "yellow";
 }
-function off(){
-    document.getElementById("bulb").style.backgroundColor = "white"
+function Off()
+{
+    document.getElementById("bulb").style.backgroundColor = "white";
 }
-function red(){
+function Red()
+{
     document.getElementById("bulb").style.backgroundColor = "red";
-}function blue(){
+}
+function Blue()
+{
     document.getElementById("bulb").style.backgroundColor = "blue";
-}function green(){
+}
+function Green()
+{
     document.getElementById("bulb").style.backgroundColor = "green";
 }
-const userColor = document.getElementById("color");
-function SB_Control(){
-    const btn = document.getElementById("SB_btn");
-    if(btn.innerText === "On"){
-        document.getElementById("SB_btn").innerText = "off";
-        document.getElementById("SmartBulb").classList.add("on")
-    } else{
-        document.getElementById("SB_btn").innerText = "on"
-        document.getElementById("SB_btn").classList.remove("off")
 
-    }
-}
+ const userColor = document.getElementById("color");
+
+ userColor.addEventListener("change",() => changeBulbColor(userColor.value));
+
+
+ function changeBulbColor(color){
+    document.getElementById("bulb").style.backgroundColor = color;
+ }
+
+
+ function SB_Control(){
+   const btn = document.getElementById("SB_btn");
+   if(btn.innerText==="On"){
+    document.getElementById("SB_btn").innerText = "Off";
+    document.getElementById("smartBulb").classList.add("on");
+   }else{
+    document.getElementById("SB_btn").innerText = "On";
+    document.getElementById("smartBulb").classList.remove("on");
+
+   }
+ }
+
+ function SB_Control2(){
+    document.getElementById("smartBulb").classList.toggle("on");
+ }
+
+ document.getElementById("c1").addEventListener("mouseover",()=>{
+    fillColor("violet")
+ })
+ document.getElementById("c1").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+
+ document.getElementById("c2").addEventListener("mouseover",()=>{
+    fillColor("indigo")
+ })
+ document.getElementById("c2").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+ document.getElementById("c3").addEventListener("mouseover",()=>{
+    fillColor("blue")
+ })
+ document.getElementById("c3").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+ document.getElementById("c4").addEventListener("mouseover",()=>{
+    fillColor("green")
+ })
+ document.getElementById("c4").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+ document.getElementById("c5").addEventListener("mouseover",()=>{
+    fillColor("yellow")
+ })
+ document.getElementById("c5").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+ document.getElementById("c6").addEventListener("mouseover",()=>{
+    fillColor("orange")
+ })
+ document.getElementById("c6").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+ document.getElementById("c7").addEventListener("mouseover",()=>{
+    fillColor("red")
+ })
+ document.getElementById("c7").addEventListener("mouseleave",()=>{
+    fillColor("white")
+ })
+
+ function fillColor(color)
+ {
+    document.getElementById("rainbowbulb").style.backgroundColor = color;
+ }
--- JavaScript/ProjectCalculator/script.js
+++ JavaScript/ProjectCalculator/script.js
@@ -1,13 +1,39 @@
-function input(char){
-    if(char === '='){
-        const exp = document.getElementById("display").value;
-        document.getElementById("display").value = eval(exp);
-    } else if (char === "C"){
-        document.getElementById("display").value = "";
-    }
-    else{
-        let exp = document.getElementById("display").value;
-        exp = exp + char;
-        document.getElementById("display").value = exp;
-    }
+function input(char) {
+  if (char === "=") {
+    const exp = document.getElementById("display").value;
+    document.getElementById("display").value = eval(exp);
+  } else if (char === "C") {
+    document.getElementById("display").value = "";
+  } else {
+    let exp = document.getElementById("display").value;
+    exp = exp + char;
+    document.getElementById("display").value = exp;
+  }
 }
+
+document.addEventListener("keydown", (abc) => {
+  console.log("Pressed Key", abc.key);
+
+  if (abc.key === "Enter") {
+    input("=");
+  } else if (
+    abc.key === "1" ||
+    abc.key === "2" ||
+    abc.key === "3" ||
+    abc.key === "4" ||
+    abc.key === "5" ||
+    abc.key === "6" ||
+    abc.key === "7" ||
+    abc.key === "8" ||
+    abc.key === "9" ||
+    abc.key === "10" ||
+    abc.key === "+" ||
+    abc.key === "-" ||
+    abc.key === "*" ||
+    abc.key === "/"
+  ) {
+    input(abc.key);
+  } else if (abc.key === "Backspace") {
+    input("C");
+  }
+});
